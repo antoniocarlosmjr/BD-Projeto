abe0689eca31e8f1dba405621795e42f05f719c1
Antonio Junior <carlos12antoni@gmail.com>
2017-09-24 01:30:15 -0300

    Banco Hospital - ajuste 8

--- src/aplicacao_banco/BancoHospital.java
+++ src/aplicacao_banco/BancoHospital.java
@@ -116,8 +116,6 @@ public void consulta1(Connection conexao) throws SQLException {
 		String sql = "SELECT id_registro, primeiro_nome, sobrenome "
 				   + "FROM hospital.medico WHERE especialidade = 'Clínico Geral'";
 		Statement comando = conexao.createStatement();
-		System.out.println("Consulta 1: ");
-		
 		ResultSet resultado = comando.executeQuery(sql);
 		ResultSetMetaData rsm = resultado.getMetaData();
 		for (int i = 1; i <= rsm.getColumnCount(); i++) {
@@ -136,38 +134,50 @@ public void consulta1(Connection conexao) throws SQLException {
 		comando.close();
 	}
 
-public void consulta2(Connection conexao) throws SQLException {
+//public void consulta2(Connection conexao) throws SQLException {
+//	String sql =  "SELECT p.num_pront, p.primeiro_nome, p.sobrenome\r\n" + 
+//			"FROM hospital.paciente as p INNER JOIN hospital.acompanhante as a \r\n" + 
+//			"	ON(p.id_acomp=a.id_acomp)";
+//	Statement comando = conexao.createStatement();
+//	ResultSet resultado = comando.executeQuery(sql);
+//	ResultSetMetaData rsm = resultado.getMetaData();
+//	for (int i = 1; i <= rsm.getColumnCount(); i++) {
+//		System.out.print(rsm.getColumnName(i) + "\t\t");
+//	}
+//	
+//	System.out.println();
+//	
+//	while (resultado.next()) {
+//		for (int i = 1; i <= rsm.getColumnCount(); i++) {
+//			String campo = resultado.getString(i);
+//			System.out.print(campo + "\t\t\t");
+//		}
+//		System.out.println();
+//	}
+//	comando.close();
+//}
+
+public void consulta2(Connection conexao) throws SQLException{
 	String sql =  "SELECT p.num_pront, p.primeiro_nome, p.sobrenome\r\n" + 
-			"FROM hospital.paciente as p INNER JOIN hospital.acompanhante as a \r\n" + 
-			"	ON(p.id_acomp=a.id_acomp)";
+	"FROM hospital.paciente as p INNER JOIN hospital.acompanhante as a \r\n" + 
+	"	ON(p.id_acomp=a.id_acomp)";
 	Statement comando = conexao.createStatement();
-	System.out.println("Consulta 2: ");
-	
 	ResultSet resultado = comando.executeQuery(sql);
-	ResultSetMetaData rsm = resultado.getMetaData();
-	for (int i = 1; i <= rsm.getColumnCount(); i++) {
-		System.out.print(rsm.getColumnName(i) + "\t\t");
-	}
-	
-	System.out.println();
-	
-	while (resultado.next()) {
-		for (int i = 1; i <= rsm.getColumnCount(); i++) {
-			String campo = resultado.getString(i);
-			System.out.print(campo + "\t\t\t");
-		}
-		System.out.println();
+	while(resultado.next()){
+		String num_pront = resultado.getString("num_pront");
+		String primeiro_nome = resultado.getString(2);
+		String sobrenome = resultado.getString(3);
+		System.out.println(num_pront + "\t\t" + primeiro_nome + "\t\t" + sobrenome);
 	}
 	comando.close();
-}
+	}
+
 
 	public void consulta3(Connection conexao) throws SQLException {
 		String sql = "SELECT m.id_registro, m.primeiro_nome, m.sobrenome, m.especialidade\r\n" + 
 				"FROM hospital.crm_validacao as crm_va  NATURAL JOIN hospital.medico AS m\r\n" + 
 				"WHERE crm_va.crmativo = true";
 		Statement comando = conexao.createStatement();
-		System.out.println("Consulta 3: ");
-		
 		ResultSet resultado = comando.executeQuery(sql);
 		ResultSetMetaData rsm = resultado.getMetaData();
 		for (int i = 1; i <= rsm.getColumnCount(); i++) {
@@ -189,8 +199,6 @@ public void consulta2(Connection conexao) throws SQLException {
 	public void consulta4(Connection conexao) throws SQLException {
 		String sql = "SELECT primeiro_nome FROM hospital.paciente WHERE primeiro_nome LIKE 'A%';";
 		Statement comando = conexao.createStatement();
-		System.out.println("Consulta 4: ");
-		
 		ResultSet resultado = comando.executeQuery(sql);
 		ResultSetMetaData rsm = resultado.getMetaData();
 		for (int i = 1; i <= rsm.getColumnCount(); i++) {
@@ -209,40 +217,75 @@ public void consulta2(Connection conexao) throws SQLException {
 		comando.close();
 	}
 	
+//	public void consulta5(Connection conexao) throws SQLException {
+//		String sql = "SELECT m.especialidade, max(salario) AS SalarioMaximo, min(salario) AS SalarioMinimo\r\n" + 
+//				"FROM hospital.medico as m NATURAL JOIN hospital.CRM_Validacao as crm\r\n" + 
+//				"WHERE crm.crmativo = true\r\n" + 
+//				"GROUP BY m.especialidade";
+//		Statement comando = conexao.createStatement();
+//		ResultSet resultado = comando.executeQuery(sql);
+//		ResultSetMetaData rsm = resultado.getMetaData();
+//		for (int i = 1; i <= rsm.getColumnCount(); i++) {
+//			System.out.print(rsm.getColumnName(i) + "\t\t");
+//		}
+//		
+//		System.out.println();
+//		
+//		while (resultado.next()) {
+//			for (int i = 1; i <= rsm.getColumnCount(); i++) {
+//				String campo = resultado.getString(i);
+//				System.out.print(campo + "\t\t\t");	
+//			}
+//			System.out.println();
+//		}
+//		comando.close();
+//		}
+	
 	public void consulta5(Connection conexao) throws SQLException {
 		String sql = "SELECT m.especialidade, max(salario) AS SalarioMaximo, min(salario) AS SalarioMinimo\r\n" + 
 				"FROM hospital.medico as m NATURAL JOIN hospital.CRM_Validacao as crm\r\n" + 
 				"WHERE crm.crmativo = true\r\n" + 
 				"GROUP BY m.especialidade";
 		Statement comando = conexao.createStatement();
-		System.out.println("Consulta 5: ");
-		
 		ResultSet resultado = comando.executeQuery(sql);
-		ResultSetMetaData rsm = resultado.getMetaData();
-		for (int i = 1; i <= rsm.getColumnCount(); i++) {
-			System.out.print(rsm.getColumnName(i) + "\t\t");
-		}
-		
-		System.out.println();
-		
+		System.out.println("especialidade" + "\t\t\t" + "salariomaximo" + "\t\t\t"  + "salariominimo");
 		while (resultado.next()) {
-			for (int i = 1; i <= rsm.getColumnCount(); i++) {
-				String campo = resultado.getString(i);
-				System.out.print(campo + "\t\t\t");
-			}
-			System.out.println();
+			String especialidade = resultado.getString("especialidade");
+			String salariomaximo = resultado.getString("salariomaximo");
+			String salariominimo = resultado.getString("salariominimo");
+			System.out.println(especialidade + "\t\t\t" + salariomaximo + "\t\t\t"  + salariominimo);
 		}
 		comando.close();
 		}
+	
+	
+	public void consulta20(Connection conexao) throws SQLException{
+		String sql = "Select cpf, salario from universidade.professor where departamento = 'DCOMP'";
+		Statement comando = conexao.createStatement();
+		System.out.println("Executar consulta: " + sql);
+		ResultSet resultado = comando.executeQuery(sql);
+		while(resultado.next()){
+			String cpf = resultado.getString("cpf");
+			String cpf2 = resultado.getString(1);
+			//String nome3 = resultado.getString(2);
+			double salario = resultado.getDouble(2);
+			System.out.println(cpf + "\t" + salario);
+		}
+		comando.close();
+		}
+	
+	
+	
+	
+	
+	
 
 public void consulta6(Connection conexao) throws SQLException {
-	String sql = "SELECT COALESCE(P.primeiro_nome, 'SEM NOME')\r\n" + 
+	String sql = "SELECT COALESCE(P.primeiro_nome, 'SEM NOME') AS nomes\r\n" + 
 			"FROM (SELECT C.paciente FROM hospital.consulta AS C INNER JOIN hospital.medico AS M\r\n" + 
 			"			ON C.medico = M.id_registro AND M.especialidade = 'Clínico Geral'\r\n" + 
 			"     ) AS dados LEFT JOIN hospital.paciente P ON dados.paciente = P.num_pront;";
 	Statement comando = conexao.createStatement();
-	System.out.println("Consulta 6: ");
-	
 	ResultSet resultado = comando.executeQuery(sql);
 	ResultSetMetaData rsm = resultado.getMetaData();
 	for (int i = 1; i <= rsm.getColumnCount(); i++) {
@@ -262,7 +305,7 @@ public void consulta6(Connection conexao) throws SQLException {
 }
 
 public void consulta7(Connection conexao) throws SQLException {
-	String sql = "SELECT med.nome, d.descricao\r\n" + 
+	String sql = "SELECT med.nome AS medicamento, d.descricao\r\n" + 
 			"FROM (SELECT *\r\n" + 
 			"	FROM hospital.medicamento AS m NATURAL JOIN hospital.prescreve AS p) AS med\r\n" + 
 			"	JOIN hospital.medico AS me USING(id_registro)\r\n" + 
@@ -270,12 +313,10 @@ public void consulta7(Connection conexao) throws SQLException {
 			"	JOIN hospital.exame AS ex ON(ex.medico=med.id_registro)\r\n" + 
 			"WHERE d.descricao <> 'Nada a constar'";
 	Statement comando = conexao.createStatement();
-	System.out.println("Consulta 7: ");
-	
 	ResultSet resultado = comando.executeQuery(sql);
 	ResultSetMetaData rsm = resultado.getMetaData();
 	for (int i = 1; i <= rsm.getColumnCount(); i++) {
-		System.out.print(rsm.getColumnName(i) + "\t\t");
+		System.out.print(rsm.getColumnName(i) + "\t\t\t");
 	}
 	
 	System.out.println();
@@ -294,13 +335,10 @@ public void consulta8(Connection conexao) throws SQLException {
 	String sql = "SELECT especialidade, count(especialidade) FROM hospital.medico GROUP BY especialidade "
 			+ "HAVING especialidade <> 'Gastro'";
 	Statement comando = conexao.createStatement();
-	System.out.println("Consulta 8: ");
-	
 	ResultSet resultado = comando.executeQuery(sql);
 	ResultSetMetaData rsm = resultado.getMetaData();
 	for (int i = 1; i <= rsm.getColumnCount(); i++) {
 		System.out.print(rsm.getColumnName(i) + "\t\t");
-		// rsm.getColumnTypeName(i)
 	}
 	
 	System.out.println();
@@ -308,7 +346,7 @@ public void consulta8(Connection conexao) throws SQLException {
 	while (resultado.next()) {
 		for (int i = 1; i <= rsm.getColumnCount(); i++) {
 			String campo = resultado.getString(i);
-			System.out.print(campo + "\t\t\t");
+			System.out.print(campo + "\t\t");
 		}
 		System.out.println();
 	}
@@ -322,8 +360,6 @@ public void consulta9(Connection conexao) throws SQLException {
 			"	FROM hospital.cadastro as c\r\n" + 
 			"	WHERE c.data_cad BETWEEN '01-01-2015' AND '31-12-2015')";
 	Statement comando = conexao.createStatement();
-	System.out.println("Consulta 9: ");
-	
 	ResultSet resultado = comando.executeQuery(sql);
 	ResultSetMetaData rsm = resultado.getMetaData();
 	for (int i = 1; i <= rsm.getColumnCount(); i++) {
@@ -348,14 +384,11 @@ public void consulta10(Connection conexao) throws SQLException {
 			"      GROUP BY especialidade) AS dados JOIN hospital.medico as m1 ON (dados.esp=m1.especialidade)\r\n" + 
 			"WHERE m1.salario >= dados.MEDIA\r\n" + 
 			"ORDER BY m1.salario ASC";
-	Statement comando = conexao.createStatement();
-	System.out.println("Consulta 10: ");
-	
+	Statement comando = conexao.createStatement();	
 	ResultSet resultado = comando.executeQuery(sql);
 	ResultSetMetaData rsm = resultado.getMetaData();
 	for (int i = 1; i <= rsm.getColumnCount(); i++) {
 		System.out.print(rsm.getColumnName(i) + "\t\t");
-		// rsm.getColumnTypeName(i)
 	}
 	
 	System.out.println();
